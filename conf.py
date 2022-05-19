6f877c98a0eff1df8b2b517c2d04c76b11f17e89
tuguerx087 <104881311+tuguerx087@users.noreply.github.com>
2022-05-19 08:40:53 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "213e85670cd03dfdcfc4936c86d153a2"
-api_id = 15091118
-bot_token = "5222714351:AAGJIy9EWsfa6HEZtsDL9IU7HxeCaGYqm1o"
-bot_admin_group = '-1001766577678'
+api_hash = "631645d79c0bfffb669b08fec9084b2f"
+api_id = 14921549
+bot_token = "5371571801:AAH98PZqsYrELL9w6leYu1wG0B-RSxK6Mjo"
+bot_admin_group = '-1001703356712'
